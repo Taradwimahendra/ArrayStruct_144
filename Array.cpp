8bccb99bd8c14d1778d18f0b2dbd5e6124dea0a3
Taradwimahendra15 <taradwimahendra15@gmail.com>
2025-04-15 19:16:52 +0700

    mengisi array dengan looping for

--- Array.cpp
+++ Array.cpp
@@ -10,3 +10,20 @@ int skor[4]={9,13,20,11};
 //deklarasi array dengan ukuran, tanpa nilai awal
 string nama[3];
 
+int mainn(){
+    cout << "Isi data skor yang ketiga = " << skor[2] << endl;
+    //mengisi array pada index tertentu secara langsung
+    skor[2] = 50;
+    cout << "Isi data skor yang ketiga = " << skor[2] << endl;
+
+    //mengisi array dengan looping for
+    for(int i = 0; i < 3; i++){
+        cout << "Mahasiswa ke-" << i+1 << " = ";
+        cin >> nama[i];
+    }
+
+    cout << endl;
+    for(int i = 0; i < 3; i++){
+        cout << "Data ke-" << i+1 << " = " << nama[i] << endl;
+    }  
+};
